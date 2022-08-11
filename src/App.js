64510b03f553ea5c92f8cb9f8f25d251f078c9e4
unknown <ukaran11@gmail.com>
2022-08-11 20:51:42 +0530

    Utsav | Without react query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Characters from './Components/Characters';
 
 function App() {
   return (
     <div className="App">
-      
+      <Characters />
     </div>
   );
 }
